1bd36e2fe6b0506b32764c9a362debac370470ce
Dima Aseev <dmiseev@gmail.com>
2018-04-20 19:06:21 +0300

    Truncate tables and load fixtures before each test

--- README.md
+++ README.md
@@ -5,3 +5,17 @@ alias dm-composer="docker-compose exec php-fpm composer"
 alias dm-php="docker-compose exec php-fpm php"
 alias dm-phpunit="docker-compose exec php-fpm vendor/bin/simple-phpunit"
 ```
+
+### Init
+
+```ini
+dm-composer install
+dm-php bin/console doctrine:migrations:migrate
+dm-php bin/console doctrine:fixtures:load
+```
+
+### Testing
+
+```ini
+dm-phpunit --stop-on-failure
+```
--- src/Tests/Acceptance/UserTest.php
+++ src/Tests/Acceptance/UserTest.php
@@ -8,13 +8,27 @@ class UserTest extends TestCase
     /**
      * @test
      */
-    public function testShouldReturnAllUsers()
+    public function testShouldReturnAllUsersList()
     {
         $response = $this->request('GET', '/v1/users', [], []);
-
         $responseBody = json_decode($response->getContent(), true);
 
-        $this->assertEquals(1, count($responseBody));
-        $this->assertEquals(1, $response->headers->get('x-items-count'));
+        $this->assertEquals(2, count($responseBody));
+        $this->assertEquals(2, $response->headers->get('x-items-count'));
+    }
+
+    /**
+     * @test
+     */
+    public function testShouldReturnUserById()
+    {
+        $response = $this->request('GET', '/v1/users/1', [], []);
+        $responseBody = json_decode($response->getContent(), true);
+
+        $this->assertEquals([
+            'id' => 1,
+            'name' => 'Alex Clare',
+            'email' => 'alex.clare@test.com'
+        ], $responseBody);
     }
 }
